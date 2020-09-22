4ac3a56365462e8d520320707bd8ac60b1f789f7
daproclaima <sn.itpro@gmail.com>
2020-09-22 11:31:41 +0200

    refactor(services.php): set default values

---
+++ config/services.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Controller\OrderController;
+use App\Database\Database;
+use App\Logger;
+use App\Mailer\GmailMailer;
+use App\Mailer\SmtpMailer;
+use App\Texter\FaxTexter;
+use App\Texter\SmsTexter;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+return function (\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $configurator) {
+    $parameters = $configurator->parameters();
+    $parameters
+        ->set('mailer.gmail_user', 'sebastien@gmail.com')
+        ->set('mailer.gmail_password', 'password');
+    
+    $services = $configurator->services();
+
+    $services->defaults()
+        ->autowire(true)
+    ;
+
+    $services
+        ->set('order_controller', OrderController::class)
+        ->public()
+        ->call('sayHello', ['Bonjour à tous', 33])
+        ->call('setSecondaryMailer', [service('mailer.gmail')])
+
+        ->set('logger', Logger::class)
+        
+        ->set('database', Database::class)
+        
+        ->set('texter.sms', SmsTexter::class)
+        ->args(["service.sms.com", "apikey123"])
+        ->tag('with_logger')
+
+        ->set('mailer.gmail', GmailMailer::class)
+        ->args(["%mailer.gmail_user%", "%mailer.gmail_password%"])
+        ->tag('with_logger')
+        
+        ->set('mailer.smtp', SmtpMailer::class)
+        ->args(['smtp:localhost', 'root', '123'])
+        
+        ->set('texter.fax', FaxTexter::class)
+        
+        
+        ->alias('App\Controller\OrderController', 'order_controller')->public()
+        ->alias('App\Database\Database', 'database')
+        ->alias('App\Mailer\GmailMailer', 'mailer.gmail')
+        ->alias('App\Mailer\SmtpMailer', 'mailer.smtp')
+        ->alias('App\Mailer\MailerInterface', 'mailer.gmail')
+        ->alias('App\Texter\SmsTexter', 'texter.sms')
+        ->alias('App\Texter\FaxTexter', 'texter.fax')
+        ->alias('App\Texter\TexterInterface', 'texter.sms')
+        ->alias('\App\Logger', 'logger')
+    ;
+};
--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
 use App\Controller\OrderController;
-use App\Database\Database;
 use App\DependencyInjection\LoggerCompilerPass;
-use App\Logger;
-use App\Mailer\GmailMailer;
-use App\Mailer\SmtpMailer;
-use App\Texter\FaxTexter;
-use App\Texter\SmsTexter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
@@ -23,39 +17,9 @@ $container = new ContainerBuilder();
 $loader = new PhpFileLoader($container, new FileLocator([__DIR__ . '/config']));
 $loader->load('services.php');
 
-//$container->autowire('logger', Logger::class);
-
-//$container->autowire('database', Database::class);
-
-//$container->autowire('texter.sms', SmsTexter::class)
-//    ->setArguments(["service.sms.com", "apikey123"])
-//    ->addTag('with_logger');
-
-//$container->autowire('mailer.gmail', GmailMailer::class)
-//    ->setArguments(["%mailer.gmail_user%", "%password%"])
-//    ->addTag('with_logger');
-//$container->setParameter('mailer.gmail_user', "snitpro@gmail.com");
-//$container->setParameter('password', 'password');
-
-//$container->autowire('mailer.smtp', SmtpMailer::class)
-//    ->setArguments(['smtp:localhost', 'root', '123']);
-//
-//$container->autowire('texter.fax', FaxTexter::class);
-
-//$container->setAlias('App\Controller\OrderController', 'order_controller')->setPublic(true);
-//$container->setAlias('App\Database\Database', 'database');
-//$container->setAlias('App\Mailer\GmailMailer', 'mailer.gmail');
-//$container->setAlias('App\Mailer\SmtpMailer', 'mailer.smtp');
-//$container->setAlias('App\Mailer\MailerInterface', 'mailer.gmail');
-//$container->setAlias('App\Texter\SmsTexter', 'texter.sms');
-//$container->setAlias('App\Texter\FaxTexter', 'texter.fax');
-//$container->setAlias('App\Texter\TexterInterface', 'texter.sms');
-//$container->setAlias('\App\Logger', 'logger');
-
 $container->addCompilerPass(new LoggerCompilerPass());
 
 $container->compile();
-
 
 $controller = $container->get(OrderController::class);
 
