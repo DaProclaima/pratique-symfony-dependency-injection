70f6eac562645e98c4ebd53f55b86603a8a0c7ee
daproclaima <sn.itpro@gmail.com>
2020-09-22 13:52:20 +0200

    refactor(index.php): creates a cached container to speed up perfs

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Controller\OrderController;
-use App\Controller\TestController;
 use App\DependencyInjection\LoggerCompilerPass;
 use App\HasLoggerInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 ini_set('display_errors', 1);
@@ -15,23 +13,39 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
+$start = microtime(true);
 $container = new ContainerBuilder();
+if( file_exists(__DIR__ . '/config/container.php')) {
+    require_once __DIR__ . '/config/container.php';
+    $container = new ProjectServiceContainer();
+} else {
+    $container->registerForAutoconfiguration(HasLoggerInterface::class)->addTag('with_logger');
 
-$container->registerForAutoconfiguration(HasLoggerInterface::class)->addTag('with_logger');
+    //$loader = new PhpFileLoader($container, new FileLocator([__DIR__ . '/config']));
+    //$loader->load('services.php');
+    $loader = new YamlFileLoader($container, new FileLocator([__DIR__ . '/config']));
+    $loader->load('services.yaml');
+    $loader->load('services2.yaml');
 
-//$loader = new PhpFileLoader($container, new FileLocator([__DIR__ . '/config']));
-//$loader->load('services.php');
-$loader = new YamlFileLoader($container, new FileLocator([__DIR__ . '/config']));
-$loader->load('services.yaml');
-$loader->load('services2.yaml');
+    $container->addCompilerPass(new LoggerCompilerPass());
 
-$container->addCompilerPass(new LoggerCompilerPass());
+    $container->compile();
 
-$container->compile();
+    $dumper = new PhpDumper($container);
+    file_put_contents(__DIR__ . '/config/container.php', $dumper->dump());
+    dump($dumper->dump());
 
-$testController = $container->get(TestController::class);
-//die();
-$controller = $container->get(OrderController::class);
+    //$testController = $container->get(TestController::class);
+    //die();
+
+}
+    $controller = $container->get('order_controller');
+
+    $duration = microtime(true) - $start;
+
+    dump("Build duration =", $duration * 1000);
+    // to speed up perfs, we gonna store compiled container in a class ProjectServiceContainer in config/container.php
+// Attention ! Any modification made in services.yaml is not considered unless we erase the cached container so that a new one with changes is created
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
