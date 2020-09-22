bba6c7e20a029db570d8a98f2992c69d17a282c1
daproclaima <sn.itpro@gmail.com>
2020-09-22 11:27:12 +0200

    feat: move services conf fromindex.php to config/services.php

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 
 use App\Controller\OrderController;
 use App\Database\Database;
+use App\DependencyInjection\LoggerCompilerPass;
+use App\Logger;
 use App\Mailer\GmailMailer;
 use App\Mailer\SmtpMailer;
 use App\Texter\FaxTexter;
 use App\Texter\SmsTexter;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -13,48 +18,41 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$container = new \Symfony\Component\DependencyInjection\ContainerBuilder();
+$container = new ContainerBuilder();
 
-$container->register('order_controller', OrderController::class)
-    ->setPublic(true)
-    ->setAutowired(true)
-    ->addMethodCall('sayHello', [
-        'Bonjour à tous',
-        33
-    ])->addMethodCall('setSecondaryMailer', [
-        new \Symfony\Component\DependencyInjection\Reference(GmailMailer::class)
-    ]);
+$loader = new PhpFileLoader($container, new FileLocator([__DIR__ . '/config']));
+$loader->load('services.php');
 
-$container->autowire('logger', \App\Logger::class);
+//$container->autowire('logger', Logger::class);
 
-$container->autowire('database', Database::class);
+//$container->autowire('database', Database::class);
 
-$container->autowire('texter.sms', SmsTexter::class)
-    ->setArguments(["service.sms.com", "apikey123"])
-    ->addTag('with_logger');
+//$container->autowire('texter.sms', SmsTexter::class)
+//    ->setArguments(["service.sms.com", "apikey123"])
+//    ->addTag('with_logger');
 
-$container->autowire('mailer.gmail', GmailMailer::class)
-    ->setArguments(["%mailer.gmail_user%", "%password%"])
-    ->addTag('with_logger');
-$container->setParameter('mailer.gmail_user', "snitpro@gmail.com");
-$container->setParameter('password', 'password');
+//$container->autowire('mailer.gmail', GmailMailer::class)
+//    ->setArguments(["%mailer.gmail_user%", "%password%"])
+//    ->addTag('with_logger');
+//$container->setParameter('mailer.gmail_user', "snitpro@gmail.com");
+//$container->setParameter('password', 'password');
 
-$container->autowire('mailer.smtp', SmtpMailer::class)
-    ->setArguments(['smtp:localhost', 'root', '123']);
+//$container->autowire('mailer.smtp', SmtpMailer::class)
+//    ->setArguments(['smtp:localhost', 'root', '123']);
+//
+//$container->autowire('texter.fax', FaxTexter::class);
 
-$container->autowire('texter.fax', FaxTexter::class);
+//$container->setAlias('App\Controller\OrderController', 'order_controller')->setPublic(true);
+//$container->setAlias('App\Database\Database', 'database');
+//$container->setAlias('App\Mailer\GmailMailer', 'mailer.gmail');
+//$container->setAlias('App\Mailer\SmtpMailer', 'mailer.smtp');
+//$container->setAlias('App\Mailer\MailerInterface', 'mailer.gmail');
+//$container->setAlias('App\Texter\SmsTexter', 'texter.sms');
+//$container->setAlias('App\Texter\FaxTexter', 'texter.fax');
+//$container->setAlias('App\Texter\TexterInterface', 'texter.sms');
+//$container->setAlias('\App\Logger', 'logger');
 
-$container->setAlias('App\Controller\OrderController', 'order_controller')->setPublic(true);
-$container->setAlias('App\Database\Database', 'database');
-$container->setAlias('App\Mailer\GmailMailer', 'mailer.gmail');
-$container->setAlias('App\Mailer\SmtpMailer', 'mailer.smtp');
-$container->setAlias('App\Mailer\MailerInterface', 'mailer.gmail');
-$container->setAlias('App\Texter\SmsTexter', 'texter.sms');
-$container->setAlias('App\Texter\FaxTexter', 'texter.fax');
-$container->setAlias('App\Texter\TexterInterface', 'texter.sms');
-$container->setAlias('\App\Logger', 'logger');
-
-$container->addCompilerPass(new \App\DependencyInjection\LoggerCompilerPass());
+$container->addCompilerPass(new LoggerCompilerPass());
 
 $container->compile();
 
